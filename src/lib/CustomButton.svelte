146bf9cda33eb071f77d0eaa6a75e529f2351f7e
Arthur <artisnard@gmail.com>
2022-10-03 14:22:21 +0200

    feature: implement Home page with register and login forms

---
+++ src/lib/CustomButton.svelte
@@ -0,0 +1,44 @@
+<script>
+  export let text;
+  export let btnType = "";
+  export let btnStyle = "primary";
+</script>
+
+<button class={btnStyle} type={btnType} on:click>
+  {text}
+</button>
+
+<style>
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+
+  button:hover {
+    border-color: #0924ef;
+  }
+
+  button.primary {
+    background-color: #5e0f4b;
+  }
+
+  button.secondary {
+    background-color: #1a1a1a;
+  }
+
+  @media (prefers-color-scheme: light) {
+    button.primary {
+      background-color: #f17dd4;
+    }
+
+    button.secondary {
+      background-color: #f9f9f9;
+    }
+  }
+</style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,83 @@
+<script>
+  import CustomButton from "../lib/CustomButton.svelte";
+  let login = true;
+  let username = "";
+  let email = "";
+  let password = "";
+
+  const toggleLogin = () => {
+    login = !login;
+  };
+</script>
+
+<main>
+  <h1>Welcome to your net worth calculator</h1>
+
+  <div class="card">
+    <h2>{login ? "Login" : "Register"}</h2>
+
+    <form>
+      {#if !login}
+        <div class="input-container">
+          <label for="username"> Username :</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            bind:value={username}
+          />
+        </div>
+      {/if}
+
+      <div class="input-container">
+        <label for="email"> Email :</label>
+        <input type="email" id="email" name="email" bind:value={email} />
+      </div>
+
+      <div class="input-container">
+        <label for="password"> Password :</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          bind:value={password}
+        />
+      </div>
+    </form>
+
+    <div class="action-container">
+      <CustomButton btnType="submit" text="Submit" />
+      <CustomButton
+        on:click={toggleLogin}
+        btnStyle="secondary"
+        btnType="submit"
+        text={login ? "Register" : "Login"}
+      />
+    </div>
+  </div>
+</main>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  .input-container {
+    width: 20rem;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+
+  input[type="text"],
+  input[type="password"],
+  input[type="email"] {
+    font-size: 1rem;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+</style>
