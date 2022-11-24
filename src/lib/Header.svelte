ed0bb369415db7e08b576c88c741603d4d5a725f
Arthur <artisnard@gmail.com>
2022-11-24 18:04:27 +0100

    wip: implementing header menu

---
+++ src/lib/Header.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import CustomButton from "./CustomButton.svelte";
+  import { currentArtist } from "../store/auth";
+
+  const logout = () => {
+    $currentArtist = null;
+  };
+</script>
+
+<header>
+  <img
+    id="logo"
+    src="/logo_loud_stream_transparent.png"
+    alt="Loud Stream logo"
+  />
+  <h1>Hello {$currentArtist.stage_name}</h1>
+  <CustomButton
+    on:click={logout}
+    btnStyle="secondary"
+    btnType="submit"
+    btnSize="small"
+    text="Logout"
+  />
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 10%;
+    background-color: var(--primary);
+    box-shadow: 0 2px 8px rgba(94, 106, 109, 0.5);
+  }
+
+  header h1 {
+    margin: 0;
+    font-size: 1.5rem;
+  }
+
+  img#logo {
+    width: 3.5rem;
+    height: 3.5rem;
+  }
+</style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,30 +1,11 @@
 <script lang="ts">
-  import CustomButton from "../lib/CustomButton.svelte";
+  import Header from "../lib/Header.svelte";
   import LatestReleases from "../lib/LatestReleases.svelte";
   import MusicPlayer from "../lib/MusicPlayer/MusicPlayer.svelte";
-  import { currentArtist } from "../store/auth";
-
-  const logout = () => {
-    $currentArtist = null;
-  };
 </script>
 
 <div class="page">
-  <header>
-    <img
-      id="logo"
-      src="/logo_loud_stream_transparent.png"
-      alt="Loud Stream logo"
-    />
-    <h1>Hello {$currentArtist.stage_name}</h1>
-    <CustomButton
-      on:click={logout}
-      btnStyle="secondary"
-      btnType="submit"
-      btnSize="small"
-      text="Logout"
-    />
-  </header>
+  <Header />
   <main>
     <h2>Latest Releases</h2>
     <LatestReleases />
@@ -39,24 +20,5 @@
 
   main {
     padding: 0 10%;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.5rem 10%;
-    background-color: var(--primary);
-    box-shadow: 0 2px 8px rgba(94, 106, 109, 0.5);
-  }
-
-  header h1 {
-    margin: 0;
-    font-size: 1.5rem;
-  }
-
-  img#logo {
-    width: 3.5rem;
-    height: 3.5rem;
   }
 </style>
